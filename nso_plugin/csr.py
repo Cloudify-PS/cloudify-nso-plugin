9f40f41cfbd7267a254995449cad962e1b8e0e7e
Isaac Shabtay <isaac@shabtay.com>
2019-10-24 23:18:51 -0700

    Initial

---
+++ nso_plugin/csr.py
@@ -0,0 +1,7 @@
+from jinja2 import Template
+
+def generate_config_drive(ctx):
+    # ctx.download_resource_and_render('resources/iosxe_config.txt', template_variables={
+    #     'host_name': host_name
+    # })
+    pass
